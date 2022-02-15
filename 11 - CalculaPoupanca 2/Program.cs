5cdd0402d4195f9816ef2d6a8a002cbf97356cf1
NicolasPassos <99737771+NicolasPassos@users.noreply.github.com>
2022-02-15 09:58:24 -0400

    Add files via upload

---
+++ 11 - CalculaPoupanca 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11___CalculaPoupanca_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando a Aula 11 Alura - Calcula Poupança 2");
+
+            double valorInvestido = 1000;
+
+            for (int contMes = 1; contMes <= 12; contMes = contMes + 1)
+            {
+                valorInvestido = valorInvestido + valorInvestido * 0.0036;
+                Console.WriteLine("Após " + contMes + " meses, você terá " + valorInvestido);
+            }
+           Console.ReadLine(); 
+        }
+    }
+}
