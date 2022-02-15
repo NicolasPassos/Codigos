61afc61e838f6ef2d6d449a3836072a68c0b6fe4
NicolasPassos <99737771+NicolasPassos@users.noreply.github.com>
2022-02-15 09:58:57 -0400

    Add files via upload

---
+++ 12 - CalculaInvestimentoLongoPrazo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12___CalculaInvestimentoLongoPrazo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando Aula 12 Alura - Calcula Investimento Longo Prazo");
+
+            double fatorRendimento = 1.0036;
+            double valorInvestido = 1000;
+
+            for (int contAno = 1; contAno <= 5; contAno++)
+            {
+                for (int contMes = 1; contMes <= 12; contMes++)
+                {
+                    valorInvestido *= fatorRendimento;
+
+                }
+                fatorRendimento += 0.0010;
+                
+            }
+
+
+            Console.WriteLine(" Ao término do investimento, você terá R$" + valorInvestido);
+            Console.ReadLine();
+        }
+    }
+}
