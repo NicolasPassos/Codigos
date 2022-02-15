8ab478d37ae88026b20a0444c15c4055a144f0c3
NicolasPassos <99737771+NicolasPassos@users.noreply.github.com>
2022-02-15 10:07:53 -0400

    Add files via upload

---
+++ 5 - ByteBank/ContaCorrente.cs
@@ -0,0 +1,43 @@
+﻿namespace _5___ByteBank
+{   
+    public class ContaCorrente
+    {
+        public Cliente titular;
+        public int agenciaConta;
+        public int numeroConta;
+        public double saldoConta;
+
+        public bool Sacar(double valorSaque)
+        {
+            if (this.saldoConta < valorSaque)
+            {
+                return false;
+            }
+            else
+            {
+                this.saldoConta -= valorSaque;
+                return true;
+            }
+        }
+        public void Depositar(double valorDeposito)
+        {
+            this.saldoConta += valorDeposito;
+        }
+
+        public bool Transferir(double valorTransferencia, ContaCorrente contaDestino)
+        {
+            if (this.saldoConta < valorTransferencia)
+            {
+                return false;
+            }
+            else
+            {
+                this.saldoConta -= valorTransferencia;
+                contaDestino.Depositar(valorTransferencia);
+
+                return true;
+
+            }
+        }
+    }
+}
---
+++ 5 - ByteBank/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5___ByteBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente nicolas = new Cliente();
+
+            nicolas.nome = "Nicolas Duarte Dos Passos";
+            nicolas.cpf = "02618720105";
+            nicolas.profissao = "Assistente de projetos";
+
+            ContaCorrente contaDoNicolas = new ContaCorrente();
+
+            contaDoNicolas.titular = nicolas;
+            contaDoNicolas.saldoConta = 1000;
+            contaDoNicolas.agenciaConta = 0001;
+            contaDoNicolas.numeroConta = 1;
+
+
+            Console.WriteLine(contaDoNicolas.titular.nome);
+
+            Console.ReadLine();
+        }
+    }
+}
