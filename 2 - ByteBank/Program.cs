8fd5b06821a761effe074163df8aa1f72b8ffb08
NicolasPassos <99737771+NicolasPassos@users.noreply.github.com>
2022-02-15 10:02:12 -0400

    Add files via upload

---
+++ 2 - ByteBank/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2___ByteBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando Aula 2 Alura - Valores Padrões");
+
+            ContaCorrente conta = new ContaCorrente();
+
+
+            Console.WriteLine(ContaCorrente.numeroConta);
+
+        }
+    }
+}
