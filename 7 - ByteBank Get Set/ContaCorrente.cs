baea39fde0693705e1cfe633a487d63aa31b4f73
NicolasPassos <99737771+NicolasPassos@users.noreply.github.com>
2022-02-15 10:08:58 -0400

    Add files via upload

---
+++ 7 - ByteBank Get Set/ContaCorrente.cs
@@ -0,0 +1,87 @@
+﻿namespace _5___ByteBank
+{
+    public class ContaCorrente
+    {
+        public Cliente titular;
+        private int _agenciaConta;
+        public int numeroConta;
+        private double _saldoConta;
+
+        public static int TotalContasCriadas { get; private set; }
+
+
+        public double Saldo
+        {
+            get
+            {
+                return _saldoConta;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+                _saldoConta = value;
+            }
+        }
+
+        public int Agencia
+        {
+            get
+            {
+                return _agenciaConta;
+            }
+            set
+            {
+                if(value <= 0)
+                { 
+                    return; 
+                }
+            }
+        }
+
+        public ContaCorrente(int agencia, int numero)
+        {
+            numeroConta = numero;
+            _agenciaConta = agencia;
+
+            TotalContasCriadas++;
+        }
+
+
+        public bool Sacar(double valorSaque)
+        {
+            if (this._saldoConta < valorSaque)
+            {
+                return false;
+            }
+            else
+            {
+                this._saldoConta -= valorSaque;
+                return true;
+            }
+        }
+        public void Depositar(double valorDeposito)
+        {
+            if (valorDeposito > 0)
+            this._saldoConta += valorDeposito;
+        }
+
+        public bool Transferir(double valorTransferencia, ContaCorrente contaDestino)
+        {
+            if (this._saldoConta < valorTransferencia)
+            {
+                return false;
+            }
+            else
+            {
+                this._saldoConta -= valorTransferencia;
+                contaDestino.Depositar(valorTransferencia);
+
+                return true;
+
+            }
+        }
+    }
+}
---
+++ 7 - ByteBank Get Set/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5___ByteBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente nicolas = new Cliente();
+
+            ContaCorrente contaDoNicolas = new ContaCorrente(100, 101010);
+            ContaCorrente contaDaClara = new ContaCorrente(110, 102010);
+            ContaCorrente contaDaDiana = new ContaCorrente(100, 100010);
+
+            contaDoNicolas.titular = nicolas;
+            //contaDoNicolas.Agencia = 56;
+            contaDoNicolas.numeroConta = 1;
+
+            contaDoNicolas.Depositar(0);
+
+
+            Console.WriteLine(contaDoNicolas.titular.Nome);
+            Console.WriteLine(ContaCorrente.TotalContasCriadas);
+            Console.WriteLine(contaDoNicolas.Agencia);
+
+            Console.ReadLine();
+        }
+    }
+}
