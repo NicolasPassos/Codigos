5a10f4c5e6ae40041bd27b2c1149cfb430d8cc89
NicolasPassos <99737771+NicolasPassos@users.noreply.github.com>
2022-02-15 09:59:34 -0400

    Add files via upload

---
+++ 13 - ForEncadeado/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13___ForEncadeado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando Aula 13 Alura - For Encadeado");
+
+            for (int contLinha = 0; contLinha < 10; contLinha++)
+            {
+                for (int contColuna = 0; contColuna < 10; contColuna++)
+                {
+                    Console.Write("*");
+                    if (contColuna >= contLinha)
+                        break;
+                }
+                Console.WriteLine();
+            }
+        Console.ReadLine();
+        }
+    }
+}
